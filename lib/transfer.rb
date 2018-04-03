91940f3b27af3bdb6cdbe841ca3a49b649e282db
Vanessa Toro <vanetoro84@gmail.com>
2018-04-03 17:36:48 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,23 @@
 class Transfer
-  # your code here
+      attr_reader :amount, :status, :sender, :receiver
+  
+   def initialize(sender, receiver, amount)
+     @amount = amount
+     @status = 'pending'
+     @receiver = receiver
+     @sender = sender
+   end
+   
+   def valid?
+     sender.valid?
+     receiver.valid?
+   end
+   
+   def execute_transaction
+     binding.pry
+    if valid?
+     receiver.deposit(amount)
+   else
+     @status = 'rejected'
+   end
 end
